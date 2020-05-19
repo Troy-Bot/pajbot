38e8f5894038b41dc8f2ca96d2e6684ec6f174f7
TroyDota <49777269+TroyDota@users.noreply.github.com>
2020-05-19 11:16:33 +0200

    TroyBot Initial Update (#1)
    TroyBot Initial Update

---
+++ pajbot/migration_revisions/db/0013_playsound.py
@@ -0,0 +1,9 @@
+import logging
+
+log = logging.getLogger("pajbot")
+
+
+def up(cursor, bot):
+    # new: tier record
+    cursor.execute('ALTER TABLE "playsound" ADD COLUMN cost integer DEFAULT NULL ')
+    cursor.execute('ALTER TABLE "playsound" ADD COLUMN tier INTEGER DEFAULT NULL')
