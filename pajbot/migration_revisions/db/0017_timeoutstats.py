43b1042435d84dfc6648254ea3ff534d66b973f0
TroyDota <49777269+TroyDota@users.noreply.github.com>
2020-05-26 09:44:57 +0200

    Update 0017_timeoutstats.py

--- pajbot/migration_revisions/db/0017_timeoutstats.py
+++ pajbot/migration_revisions/db/0017_timeoutstats.py
@@ -12,7 +12,7 @@ def up(cursor, bot):
         SELECT
             id as user_id,
             RANK() OVER (ORDER BY points DESC) points_rank,
-            RANK() OVER (ORDER BY num_lines DESC) num_lines_rank
+            RANK() OVER (ORDER BY num_lines DESC) num_lines_rank,
             RANK() OVER (ORDER BY num_paid_timeouts DESC) num_paid_timeouts_rank
         FROM "user"
     )
